c0bc02b6fb1da7e8ab8bd36899e4894e92baf045
DUONG <duonghovo@gmail.com>
2023-06-18 19:50:14 +0700

    Tiện ích và bảng giá

--- src/pages/EnterIndexPage/EnterIndexPage.jsx
+++ src/pages/EnterIndexPage/EnterIndexPage.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { initialData } from "./data";
 import { otherData } from "./otherData.js";
+import WaterPriceTable from "./WaterPriceTable";
 import viVN from "antd/es/date-picker/locale/vi_VN";
 import { updateSearchCriteria } from "../../redux/enterIndexPage/searchCriteriaSlice";
 import {
+  Dropdown,
+  Menu,
   Form,
   Input,
   Button,
@@ -20,7 +23,18 @@ import {
   Upload,
   Progress,
 } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import {
+  SearchOutlined,
+  DownloadOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  PlusCircleOutlined,
+  CalculatorOutlined,
+  SyncOutlined,
+  RetweetOutlined,
+  CheckCircleOutlined,
+  CalendarOutlined,
+} from "@ant-design/icons";
 import moment from "moment";
 import "./EnterIndexPage.css";
 import "moment/locale/vi";
@@ -44,6 +58,12 @@ function EnterIndexPage() {
   const handleButtonClick2 = () => {
     setIsModalVisible2(true);
   };
+  const handleButtonClick3 = () => {
+    setIsModalVisible3(true);
+  };
+  const handleButtonClick4 = () => {
+    setIsModalVisible4(true);
+  };
   const handleModalCancel = () => {
     setIsModalVisible(false);
   };
@@ -52,6 +72,12 @@ function EnterIndexPage() {
   };
   const handleModalCancel2 = () => {
     setIsModalVisible2(false);
+  };
+  const handleModalCancel3 = () => {
+    setIsModalVisible3(false);
+  };
+  const handleModalCancel4 = () => {
+    setIsModalVisible4(false);
   };
   const onFinish = (values) => {
     dispatch(updateSearchCriteria(values));
@@ -69,6 +95,8 @@ function EnterIndexPage() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalVisible1, setIsModalVisible1] = useState(false);
   const [isModalVisible2, setIsModalVisible2] = useState(false);
+  const [isModalVisible3, setIsModalVisible3] = useState(false);
+  const [isModalVisible4, setIsModalVisible4] = useState(false);
 
   const { Option } = Select;
   const [data1, setData1] = useState(initialData);
@@ -291,6 +319,9 @@ function EnterIndexPage() {
     const onFinish = (values) => {
       console.log("Received values of form: ", values);
     };
+    const handleReset = () => {
+      form.resetFields();
+    };
 
     return (
       <Form
@@ -341,9 +372,8 @@ function EnterIndexPage() {
           >
             <Form.Item size="small" label="Cán bộ đọc" name="1">
               <Select size="small" name="s1">
-                <Option value="1">Lựa chọn 1</Option>
-                <Option value="2">Lựa chọn 2</Option>
-                <Option value="3">Lựa chọn 3</Option>
+                <Option value="1"> 1</Option>
+                <Option value="2"> 2</Option>
               </Select>
             </Form.Item>
           </Col>
@@ -362,9 +392,9 @@ function EnterIndexPage() {
               name="2"
             >
               <Select size="small" name="s1">
-                <Option value="1">Lựa chọn 1</Option>
-                <Option value="2">Lựa chọn 2</Option>
-                <Option value="3">Lựa chọn 3</Option>
+                <Option value="1">1</Option>
+                <Option value="2">2</Option>
+                <Option value="3">3</Option>
               </Select>
             </Form.Item>
           </Col>
@@ -453,9 +483,9 @@ function EnterIndexPage() {
               name="6"
             >
               <Select size="small" name="s1" dropdownStyle={{ width: "200px" }}>
-                <Option value="1">Lựa chọn 1</Option>
-                <Option value="2">Lựa chọn 2</Option>
-                <Option value="3">Lựa chọn 3</Option>
+                <Option value="1"> 1</Option>
+                <Option value="2"> 2</Option>
+                <Option value="3"> 3</Option>
               </Select>
             </Form.Item>
           </Col>
@@ -576,6 +606,50 @@ function EnterIndexPage() {
       borderRadius: token.borderRadiusLG,
       padding: 24,
     };
+    const handleFinish = (values) => {
+      console.log(values);
+      // Xử lý dữ liệu tìm kiếm tại đây
+    };
+
+    const handleReset = () => {
+      form.resetFields();
+    };
+
+    const { Option } = Select;
+    const menu = (
+      <Menu>
+        <Menu.Item key="1" icon={<DownloadOutlined />}>
+          Xuất excel
+        </Menu.Item>
+        <Menu.Item key="2" icon={<EditOutlined />}>
+          Cập nhật sản lượng tạm tính
+        </Menu.Item>
+        <Menu.Item key="3" icon={<DeleteOutlined />}>
+          Cập nhật hủy hóa đơn
+        </Menu.Item>
+        <Menu.Item key="4" icon={<PlusCircleOutlined />}>
+          Nhập truy thu
+        </Menu.Item>
+        <Menu.Item key="5" icon={<CalculatorOutlined />}>
+          Nhập chỉ số
+        </Menu.Item>
+        <Menu.Item key="6" icon={<SyncOutlined />}>
+          Nhập tiêu thụ
+        </Menu.Item>
+        <Menu.Item key="7" icon={<RetweetOutlined />}>
+          Thay đồng hồ
+        </Menu.Item>
+        <Menu.Item key="8" icon={<CheckCircleOutlined />}>
+          Quay đồng hồ
+        </Menu.Item>
+        <Menu.Item key="9" icon={<CalendarOutlined />}>
+          Gán chỉ số cuối
+        </Menu.Item>
+        <Menu.Item key="10" icon={<CalendarOutlined />}>
+          Gán ngày cuối
+        </Menu.Item>
+      </Menu>
+    );
     return (
       <Form
         form={form}
@@ -588,6 +662,178 @@ function EnterIndexPage() {
         <div
           style={{ display: "flex", marginTop: "10px", paddingRight: "10px" }}
         >
+          <Button onClick={handleButtonClick3} icon={<SearchOutlined />}>
+            Tìm kiếm
+          </Button>
+          <Modal
+            title="Tìm kiếm nhập chỉ số nâng cao"
+            visible={isModalVisible3}
+            onCancel={handleModalCancel3}
+            footer={null}
+          >
+            <Form form={form} onFinish={handleFinish}>
+              <Row>
+                <Col span={12}>
+                  <Form.Item label="Loại khách hàng" name="customerType">
+                    <Select>
+                      {/* Thêm các lựa chọn loại khách hàng tại đây */}
+                      <Option value="1">Loại 1</Option>
+                      <Option value="2">Loại 2</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Phạm vi" name="range">
+                    <Select>
+                      {/* Thêm các lựa chọn phạm vi tại đây */}
+                      <Option value="1">Phạm vi 1</Option>
+                      <Option value="2">Phạm vi 2</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <Form.Item label="Vùng" name="region">
+                    <Select>
+                      {/* Thêm các lựa chọn vùng tại đây */}
+                      <Option value="1">Vùng 1</Option>
+                      <Option value="2">Vùng 2</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Khu vực" name="area">
+                    <Select>
+                      {/* Thêm các lựa chọn khu vực tại đây */}
+                      <Option value="1">Khu vực 1</Option>
+                      <Option value="2">Khu vực 2</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <Form.Item label="Ngày đọc từ" name="fromDate">
+                    <DatePicker
+                      locale={viVN}
+                      size="small"
+                      style={{
+                        width: "100%",
+                      }}
+                      onChange={handleMonthChange}
+                      format="DD/MM/YYYY"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item label="Đến" name="toDate">
+                    <DatePicker
+                      locale={viVN}
+                      size="small"
+                      style={{
+                        width: "100%",
+                      }}
+                      onChange={handleMonthChange}
+                      format="DD/MM/YYYY"
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              {/* Nút Tìm kiếm, Xóa điều kiện TK và Đóng */}
+              <Row justify="end">
+                <Button type="primary" htmlType="submit">
+                  Tìm kiếm
+                </Button>{" "}
+                &nbsp;
+                <Button onClick={handleReset}>Xóa điều kiện TK</Button> &nbsp;
+                <Button onClick={handleModalCancel3}>Đóng</Button> &nbsp;
+              </Row>
+            </Form>
+          </Modal>
+          <div className="button-container1">
+            <Button size="small" onClick={handleButtonClick1}>
+              Nhập excel
+            </Button>
+          </div>
+          <Modal
+            title="Nhập execel"
+            visible={isModalVisible1}
+            onCancel={handleModalCancel1}
+            footer={null}
+          >
+            <Row justify="end">
+              <Button>Cập Nhật</Button>
+              <Button onClick={handleModalCancel1}>Đóng</Button>
+            </Row>
+          </Modal>
+          <div className="button-container2">
+            <Button size="small" onClick={handleButtonClick2}>
+              Nhập tệp
+            </Button>
+          </div>
+          <Modal
+            title="Nhập chỉ số từ tệp"
+            visible={isModalVisible2}
+            onCancel={handleModalCancel2}
+            footer={null}
+          >
+            <Row>
+              <Col span={12}>
+                Chọn tệp:
+                <Upload>
+                  <Button>Chọn tệp từ thư mục máy</Button>
+                </Upload>
+              </Col>
+              <Col span={12}>
+                Tháng:
+                <br />
+                <DatePicker
+                  locale={viVN}
+                  size="small"
+                  style={{
+                    width: "100%",
+                  }}
+                  picker="month"
+                  onChange={handleMonthChange}
+                  format="MM/YYYY"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                Dữ liệu chỉ số trong tệp:
+                <br />
+                <Input.TextArea rows={4} />
+              </Col>
+            </Row>
+            <Row justify="end">
+              <Button>Cập Nhật</Button>
+              <Button onClick={handleModalCancel2}>Đóng</Button>
+            </Row>
+          </Modal>
+          <Dropdown overlay={menu}>
+            <Button>Tiện ích</Button>
+          </Dropdown>
+          <div className="button-container1">
+            <Button size="small" onClick={handleButtonClick4}>
+              Bảng giá
+            </Button>
+          </div>
+          <Modal
+            title="Bảng giá"
+            visible={isModalVisible4}
+            onCancel={handleModalCancel4}
+            footer={null}
+          >
+            {/* Sử dụng thành phần WaterPriceTable để hiển thị bảng giá nước */}
+            <WaterPriceTable />
+            <Row justify="end">
+              <Button>Cập Nhật</Button>
+              <Button onClick={handleModalCancel4}>Đóng</Button>
+            </Row>
+          </Modal>
           <div className="button-container">
             <Button size="small" onClick={handleButtonClick}>
               Xem TH SD
@@ -708,77 +954,6 @@ function EnterIndexPage() {
               </Button>
             </Row>
           </Modal>
-          <div className="button-container1">
-            <Button size="small" onClick={handleButtonClick1}>
-              Xuất excel
-            </Button>
-          </div>
-          <Modal
-            title="Xuất execel"
-            visible={isModalVisible1}
-            onCancel={handleModalCancel1}
-            footer={null}
-          >
-            <Row justify="end">
-              <Button>Cập Nhật</Button>
-              <Button onClick={handleModalCancel1}>Đóng</Button>
-            </Row>
-          </Modal>
-          <div className="button-container2">
-            <Button size="small" onClick={handleButtonClick2}>
-              Nhập tệp
-            </Button>
-          </div>
-          <Modal
-            title="Nhập chỉ số từ tệp"
-            visible={isModalVisible2}
-            onCancel={handleModalCancel2}
-            footer={null}
-          >
-            <Row>
-              <Col span={12}>
-                Chọn tệp:
-                <Upload>
-                  <Button>Chọn tệp từ thư mục máy</Button>
-                </Upload>
-              </Col>
-              <Col span={12}>
-                Tháng:
-                <br />
-                <DatePicker
-                  locale={viVN}
-                  size="small"
-                  style={{
-                    width: "100%",
-                  }}
-                  picker="month"
-                  onChange={handleMonthChange}
-                  format="MM/YYYY"
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
-                Dữ liệu chỉ số trong tệp:
-                <br />
-                <Input.TextArea rows={4} />
-              </Col>
-            </Row>
-            <Row justify="end">
-              <Button>Cập Nhật</Button>
-              <Button onClick={handleModalCancel2}>Đóng</Button>
-            </Row>
-          </Modal>
-          <Form.Item size="small" className="custom-form-item">
-            <Button
-              size="small"
-              className="small-button"
-              type="primary"
-              htmlType="submit"
-            >
-              <SearchOutlined />
-            </Button>
-          </Form.Item>
 
           {/* <div className="button-container">
         <Button onClick={handleButtonClick}>
