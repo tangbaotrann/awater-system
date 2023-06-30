7f8f200b1f2379722c44525c7943e0e02d05c558
Nguyen Xuan Tam <nguyenxuantam2611@gmail.com>
2023-06-30 16:37:06 +0700

    modal price list

--- src/pages/Category/ManagementPriceList/components/ModalAddPriceList/ModalAddPriceList.jsx
+++ src/pages/Category/ManagementPriceList/components/ModalAddPriceList/ModalAddPriceList.jsx
@@ -7,17 +7,78 @@ import {
   Modal,
   Select,
   Space,
+  Table,
 } from "antd";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./ModalAddPriceList.css";
-import { CloseCircleOutlined, SaveOutlined } from "@ant-design/icons";
-const ModalAddPriceList = ({
-  isOpen,
-  handleCancel,
-  handleOk,
-  openTransferManagers,
-}) => {
+import {
+  CloseCircleOutlined,
+  CloseCircleTwoTone,
+  PlusCircleTwoTone,
+  SaveOutlined,
+} from "@ant-design/icons";
+
+const priceListColumns = [
+  {
+    key: "key",
+    title: "#",
+    dataIndex: "key",
+    width: "4vw",
+  },
+  {
+    key: "index",
+    title: "STT",
+    dataIndex: "index",
+    width: "4vw",
+    align: "center",
+  },
+  {
+    key: "describe",
+    title: "Mô tả CT",
+    dataIndex: "describe",
+    width: "15vw",
+  },
+  {
+    key: "fromNumber",
+    title: "Từ số",
+    dataIndex: "fromNumber",
+    width: "10vw",
+  },
+  {
+    key: "toNumber",
+    title: "Đến số",
+    dataIndex: "toNumber",
+    width: "8vw",
+  },
+  {
+    key: "vat",
+    title: "Giá có VAT",
+    dataIndex: "vat",
+    width: "8vw",
+  },
+  {
+    key: "price",
+    title: "Giá",
+    dataIndex: "price",
+    width: "8vw",
+  },
+];
+
+const priceListData = [];
+for (let i = 0; i <= 5; i++) {
+  priceListData.push({
+    key: i,
+    index: i,
+    describe: `Mô tả CT${i}`,
+    fromNumber: `Từ số ${i}`,
+    toNumber: `Đến số ${i}`,
+    vat: `Giá VAT ${i}`,
+    price: `Giá ${i}`,
+  });
+}
+
+const ModalAddPriceList = ({ isOpen, handleCancel, handleOk }) => {
   const tabList = useSelector((state) => state.tabListContractSlice.tabList);
   const priceObjectOptions = [];
   for (let i = 1; i < 36; i++) {
@@ -30,144 +91,175 @@ const ModalAddPriceList = ({
   const dateFormatList = ["DD/MM/YYYY", "DD/MM/YY", "DD-MM-YYYY", "DD-MM-YY"];
 
   useEffect(() => {
-    form.setFieldValue("oldEmployee", tabList?.cashier || "");
-    form.setFieldValue("codeLine", tabList?.codeLine || "");
-    form.setFieldValue("nameLine", tabList?.nameLine || "");
-  }, [tabList, openTransferManagers]);
+    form.setFieldValue("priceObject", tabList?.priceObject || "");
+    // form.setFieldValue("fromDate", tabList?.fromDate || "");
+    // form.setFieldValue("toDate", tabList?.toDate || "");
+  }, [tabList]);
   const handleChangeValue = (e) => {
     form.setFieldValue(e.target.name, e.target.value);
   };
   useEffect(() => {
     if (!isOpen) return;
     return () => {
-      form.setFieldValue("oldEmployee", "");
-      form.setFieldValue("codeLine", "");
-      form.setFieldValue("nameLine", "");
+      form.setFieldValue("priceObject", "");
+      // form.setFieldValue("fromDate", "");
+      // form.setFieldValue("toDate", "");
     };
   }, [isOpen]);
 
   return (
     <Modal
-      // title={
-      //   openTransferManagers
-      //     ? "Chuyển cán bộ quản lý tuyến"
-      //     : "Chuyển cán bộ quản lý thu"
-      // }
+      title={!tabList ? "Thêm dữ liệu" : "Sửa dữ liệu"}
       open={isOpen}
       onOk={() => handleOk("addPriceList")}
       onCancel={() => handleCancel("addPriceList")}
       className="popup-add-price-list"
-      width={1000}
+      bodyStyle={{ height: "800px", overflow: "auto" }}
+      width={1100}
       footer={null}
     >
       <Form
+        className="form-price-list"
         form={form}
         name="basic"
-        labelCol={{ span: 7 }}
-        wrapperCol={{ span: 15 }}
-        style={{ maxWidth: 1000 }}
+        style={{ maxWidth: 1100 }}
         initialValues={{ remember: true }}
         autoComplete="off"
       >
-        <Form.Item
-          label="DT giá:"
-          name="priceObject"
-          rules={[{ required: true, message: "Hãy chọn đối tượng giá!" }]}
-        >
-          <Select
-            name="priceObject"
-            size="middle"
-            placeholder="Chọn đối tượng tính giá"
-            style={{ width: "100%" }}
-            options={priceObjectOptions}
-          />
-        </Form.Item>
-        <div className="form-item-wrap">
-          <div className="form-item-left">
+        <div className="price-list-info">
+          <h4 className="title">Thông tin bảng giá</h4>
+          <div className="price-list-info-item">
             <Form.Item
-              label="Ngày BĐ:"
-              name="fromDate"
-              rules={[{ required: true, message: "Hãy chọn ngày bắt đầu!" }]}
+              label="DT giá:"
+              name="priceObject"
+              rules={[{ required: true, message: "Hãy chọn đối tượng giá!" }]}
             >
-              <DatePicker
-                name="fromDate"
-                placeholder="Chọn ngày bắt đầu"
-                style={{ width: "100%" }}
-                format={dateFormatList}
-              />
-            </Form.Item>
-            <Form.Item label="Có VAT:" name="taxVAT">
-              <Checkbox style={{ marginRight: "5px" }} />
-            </Form.Item>
-            <Form.Item label="ĐT tính giá:" name="pricingPriceObject">
               <Select
-                name="pricingPriceObject"
+                name="priceObject"
                 size="middle"
                 placeholder="Chọn đối tượng tính giá"
                 style={{ width: "100%" }}
                 options={priceObjectOptions}
               />
             </Form.Item>
-            <Form.Item label="Có tối thiểu:" name="minimum">
-              <Checkbox style={{ marginRight: "5px" }} />
-            </Form.Item>
-          </div>
-          <div className="form-item-right">
-            <Form.Item
-              label="Ngày KT:"
-              name="toDate"
-              rules={[{ required: true, message: "Hãy chọn ngày kết thúc!" }]}
-            >
-              <DatePicker
-                name="toDate"
-                placeholder="Chọn ngày kết thúc"
-                style={{ width: "100%" }}
-                format={dateFormatList}
-              />
-            </Form.Item>
-            <Form.Item label="Phí BVMT:" name="feeBVMT">
-              <Select
-                name="feeBVMT"
-                size="middle"
-                placeholder="Chọn "
-                style={{ width: "100%" }}
-                options={priceObjectOptions}
-              />
-              <InputNumber min={1} size="middle" style={{ width: "100%" }} />
-            </Form.Item>
-            <Form.Item label="Kiểu tính:" name="calculationType">
-              <Select
-                name="calculationType"
-                size="middle"
-                placeholder="Chọn kiểu tính"
-                style={{ width: "100%" }}
-                options={priceObjectOptions}
-              />
-            </Form.Item>
+            <div className="form-item-wrap">
+              <div className="form-item-left">
+                <Form.Item
+                  label="Ngày BĐ:"
+                  name="fromDate"
+                  rules={[
+                    { required: true, message: "Hãy chọn ngày bắt đầu!" },
+                  ]}
+                >
+                  <DatePicker
+                    name="fromDate"
+                    placeholder="Chọn ngày bắt đầu"
+                    style={{ width: "100%" }}
+                    format={dateFormatList}
+                  />
+                </Form.Item>
+                <Form.Item label="Có VAT:" name="taxVAT">
+                  <Checkbox style={{ marginRight: "5px" }} />
+                </Form.Item>
+                <Form.Item label="ĐT tính giá:" name="pricingPriceObject">
+                  <Select
+                    name="pricingPriceObject"
+                    size="middle"
+                    placeholder="Chọn đối tượng tính giá"
+                    style={{ width: "100%" }}
+                    options={priceObjectOptions}
+                  />
+                </Form.Item>
+                <Form.Item label="Có tối thiểu:" name="minimum">
+                  <Checkbox style={{ marginRight: "5px" }} />
+                </Form.Item>
+              </div>
+              <div className="form-item-right">
+                <Form.Item
+                  label="Ngày KT:"
+                  name="toDate"
+                  rules={[
+                    { required: true, message: "Hãy chọn ngày kết thúc!" },
+                  ]}
+                >
+                  <DatePicker
+                    name="toDate"
+                    placeholder="Chọn ngày kết thúc"
+                    style={{ width: "100%" }}
+                    format={dateFormatList}
+                  />
+                </Form.Item>
+                <Form.Item label="Phí BVMT:" name="feeBVMT">
+                  <div style={{ display: "flex", gap: "5px" }}>
+                    <Select
+                      name="feeBVMT"
+                      size="middle"
+                      placeholder="Chọn "
+                      style={{ width: "100%" }}
+                      options={priceObjectOptions}
+                    />
+                    <InputNumber
+                      min={1}
+                      size="middle"
+                      style={{ width: "100%" }}
+                    />
+                  </div>
+                </Form.Item>
+                <Form.Item label="Kiểu tính:" name="calculationType">
+                  <Select
+                    name="calculationType"
+                    size="middle"
+                    placeholder="Chọn kiểu tính"
+                    style={{ width: "100%" }}
+                    options={priceObjectOptions}
+                  />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="form-item-bottom">
+              <Form.Item label="Phí duy trì:" name="maintainingFee">
+                <Select
+                  name="maintainingFee"
+                  size="middle"
+                  placeholder="Chọn đối phí duy trì"
+                  style={{ width: "100%" }}
+                  options={priceObjectOptions}
+                />
+              </Form.Item>
+              <Form.Item name="number">
+                <InputNumber min={1} size="middle" style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item label="Áp VAT:" name="minimum">
+                <Checkbox style={{ marginRight: "5px" }} />
+              </Form.Item>
+              <Form.Item name="number">
+                <InputNumber min={1} size="middle" style={{ width: "100%" }} />
+              </Form.Item>
+              <Form.Item label="Áp dụng khi tiêu thụ <=:" name="maintainingFee">
+                <InputNumber min={1} size="middle" style={{ width: "100%" }} />
+              </Form.Item>
+            </div>
           </div>
         </div>
-        <div className="form-item-bottom">
-          <Form.Item label="Phí duy trì:" name="maintainingFee">
-            <Select
-              name="maintainingFee"
-              size="middle"
-              placeholder="Chọn đối phí duy trì"
+        <div className="price-list-table">
+          <div className="table">
+            <h4>Danh sách chi tiết giá</h4>
+            <Table
+              dataSource={priceListData}
+              columns={priceListColumns}
+              rowKey="key"
               style={{ width: "100%" }}
-              options={priceObjectOptions}
+              scroll={{
+                y: 520,
+                x: 1200,
+                scrollToFirstRowOnChange: true,
+              }}
             />
-          </Form.Item>
-          <Form.Item name="number">
-            <InputNumber min={1} size="middle" style={{ width: "100%" }} />
-          </Form.Item>
-          <Form.Item label="Áp VAT:" name="minimum">
-            <Checkbox style={{ marginRight: "5px" }} />
-          </Form.Item>
-          <Form.Item name="number">
-            <InputNumber min={1} size="middle" style={{ width: "100%" }} />
-          </Form.Item>
-          <Form.Item label="Áp dụng khi tiêu thụ <=:" name="maintainingFee">
-            <InputNumber min={1} size="middle" style={{ width: "100%" }} />
-          </Form.Item>
+          </div>
+          <div className="button-actions-table">
+            <PlusCircleTwoTone />
+            <CloseCircleTwoTone />
+          </div>
         </div>
         <Form.Item className="form-item-button">
           <Space size={5} className="modal-button-actions">
