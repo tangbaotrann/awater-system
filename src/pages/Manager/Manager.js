33d256d252ce9027818d87f0d914bc76b94a970a
tangbaotrann <tangbaotrann@gmail.com>
2023-07-01 19:39:23 +0700

    updated: when clicked menu auto close + refress page old + change text color of table

--- src/pages/Manager/Manager.js
+++ src/pages/Manager/Manager.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import DefaultLayout from "../../layouts/DefaultLayout/DefaultLayout";
@@ -16,46 +17,60 @@ import ManagementPriceList from "../Category/ManagementPriceList/ManagementPrice
 import Payment from "./Payment/Payment";
 import BlockClock from "../BlockClock/BlockClock";
 import LogNotice from "../LogNotice/LogNotice";
+
 function Manager() {
+  const currentPage = sessionStorage.getItem("currentPage");
+
   // change layout
   const sidebarMenu = useSelector(btnClickSidebarMenuSelector);
 
   return (
-    <DefaultLayout>
-      {sidebarMenu === constants.CONTRACT_MANAGER.key ? ( // menu 1
+    <DefaultLayout currentPage={currentPage}>
+      {sidebarMenu === constants.CONTRACT_MANAGER.key || // menu 1
+      currentPage === constants.CONTRACT_MANAGER.key ? (
         <Contract />
-      ) : sidebarMenu === constants.NUMBER_MANAGEMENT.key ? (
-        <h1>Số quản lý đồng hồ</h1>
-      ) : sidebarMenu === constants.REGISTRATION_OF_INSTALL.key ? ( // sub menu 1.1
-        <h1>REGISTRATION_OF_INSTALL</h1>
-      ) : sidebarMenu === constants.DEV_CUSTOMER.key ? (
+      ) : sidebarMenu === constants.DEV_CUSTOMER.key || // sub menu 1.1
+        currentPage === constants.DEV_CUSTOMER.key ? (
         <CustomerDevelop />
-      ) : sidebarMenu === constants.LIST_CUSTOMER.key ? (
+      ) : sidebarMenu === constants.LIST_CUSTOMER.key ||
+        currentPage === constants.LIST_CUSTOMER.key ? (
         <CustomerList />
-      ) : sidebarMenu === constants.RECORD_INDEX_PARENT.key ? ( // menu 2
+      ) : sidebarMenu === constants.RECORD_INDEX_PARENT.key ||
+        currentPage === constants.RECORD_INDEX_PARENT.key ? ( // menu 2
         <h1>RECORD_INDEX_PARENT</h1>
-      ) : sidebarMenu === constants.READINGS_INDEX.key ? (
+      ) : sidebarMenu === constants.READINGS_INDEX.key ||
+        currentPage === constants.READINGS_INDEX.key ? (
         <ReadingIndex />
-      ) : sidebarMenu === constants.ENTER_INDEX.key ? (
+      ) : sidebarMenu === constants.ENTER_INDEX.key ||
+        currentPage === constants.ENTER_INDEX.key ? (
         <EnterIndexPage />
-      ) : sidebarMenu === constants.BILL_ORDER.key ? (
+      ) : sidebarMenu === constants.BILL_ORDER.key ||
+        currentPage === constants.BILL_ORDER.key ? (
         <Invoice />
-      ) : sidebarMenu === constants.PRINT_BILL_ORDER.key ? (
+      ) : sidebarMenu === constants.PRINT_BILL_ORDER.key ||
+        currentPage === constants.PRINT_BILL_ORDER.key ? (
         <InvoicePrint />
-      ) : sidebarMenu === constants.EXPORT_PRINT_BILL_ORDER.key ? (
+      ) : sidebarMenu === constants.EXPORT_PRINT_BILL_ORDER.key ||
+        currentPage === constants.EXPORT_PRINT_BILL_ORDER.key ? (
         <h1>EXPORT_PRINT_BILL_ORDER</h1>
-      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_PRICE_SUBJECT.key ? ( //menu 3
+      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_PRICE_SUBJECT.key ||
+        currentPage === constants.CATEGORY_MANAGEMENT_PRICE_SUBJECT.key ? ( //menu 3
         <ManagementPriceSubject />
-      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_READING.key ? (
+      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_READING.key ||
+        currentPage === constants.CATEGORY_MANAGEMENT_READING.key ? (
         <ManagementReading />
-      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_PRICE_LIST.key ? (
+      ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_PRICE_LIST.key ||
+        currentPage === constants.CATEGORY_MANAGEMENT_PRICE_LIST.key ? (
         <ManagementPriceList />
-      ) : sidebarMenu === constants.PAYMENT.key ? ( // menu 4
+      ) : sidebarMenu === constants.PAYMENT.key ||
+        currentPage === constants.PAYMENT.key ? ( // menu 4
         <Payment />
-      ) : sidebarMenu === constants.BLOCK_CLOCK_MANAGEMENT.key ? ( // menu 5
+      ) : sidebarMenu === constants.BLOCK_CLOCK_MANAGEMENT.key ||
+        currentPage === constants.BLOCK_CLOCK_MANAGEMENT.key ? ( // menu 5
         <BlockClock />
-      ) : sidebarMenu === constants.LOG_NOTICE.key ? ( // menu 6
-        <LogNotice/>
+      ) : sidebarMenu === constants.LOG_NOTICE.key ||
+        currentPage === constants.LOG_NOTICE.key ? ( // menu 6
+        <LogNotice />
       ) : null}
     </DefaultLayout>
   );
