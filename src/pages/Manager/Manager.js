e3f6a22b68e055082d6deb337874f5bbb337a43a
duonghovo <duonghovo@gmail.com>
2023-07-14 00:26:10 +0700

    fix

--- src/pages/Manager/Manager.js
+++ src/pages/Manager/Manager.js
@@ -18,10 +18,11 @@ import BlockClock from "../BlockClock/BlockClock";
 import LogNotice from "../LogNotice/LogNotice";
 import ListLocation from "../Category/List_Location/List_Location.jsx";
 import ListRegionsLocation from "../Category/ListRegionsLocation/ListRegionsLocation";
-import ListPriceObject from "../Category/ListPriceObject/ListPriceObject";import ListPaymentMethod from "../Category/List_Payment_Method/List_Payment_Method";
-import CustomerType from "../Category/Category_Customer_Type/CustomerType";
-import List_Location from "../Category/List_Location/List_Location.jsx";
-
+import ListPriceObject from "../Category/ListPriceObject/ListPriceObject";
+import ListScope from "../Category/ListScope/ListScope";
+import ListReasons from "../Category/ListReasons/ListReasons";
+import ListCancel from "../Category/ListCancel/ListCancel";
+import ListWatch from "../Category/ListWatch/ListWatch";
 function Manager() {
   const currentPage = sessionStorage.getItem("currentPage");
 
@@ -69,16 +70,24 @@ function Manager() {
         <ManagementPriceList />
       ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_LRL.key ||
         currentPage === constants.CATEGORY_MANAGEMENT_LRL.key ? (
+        // <h1>Danh muc vung</h1>
         <ListRegionsLocation />
       ) : sidebarMenu === constants.CATEGORY_MANAGEMENT_LOCATION.key ||
         currentPage === constants.CATEGORY_MANAGEMENT_LOCATION.key ? (
-        <List_Location />
-      ) : sidebarMenu === constants.CATEGORY_PAYMENT_METHOD.key ||
-        currentPage === constants.CATEGORY_PAYMENT_METHOD.key ? (
-        <ListPaymentMethod />
-      ) : sidebarMenu === constants.CATEGORY_CUSTOMER_TYPE.key ||
-        currentPage === constants.CATEGORY_CUSTOMER_TYPE.key ? (
-        <CustomerType />
+        // <h1>Danh muc vung</h1>
+        <ListLocation />
+      ) : sidebarMenu === constants.CATEGORY_SCOPE.key ||
+        currentPage === constants.CATEGORY_SCOPE.key ? (
+        <ListScope />
+      ) : sidebarMenu === constants.CATEGORY_MANAGEMEN_REASONS_INSTEAD.key ||
+        currentPage === constants.CATEGORY_MANAGEMEN_REASONS_INSTEAD.key ? (
+        <ListReasons />
+      ) : sidebarMenu === constants.CATEGORY_MANAGEMEN_REASONS_CANCEL.key ||
+        currentPage === constants.CATEGORY_MANAGEMEN_REASONS_CANCEL.key ? (
+        <ListCancel />
+      ) : sidebarMenu === constants.CATEGORY_WATCH.key ||
+        currentPage === constants.CATEGORY_WATCH.key ? (
+        <ListWatch />
       ) : sidebarMenu === constants.PAYMENT.key ||
         currentPage === constants.PAYMENT.key ? ( // menu 4
         <Payment />
