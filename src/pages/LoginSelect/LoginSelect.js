19393ddd55cac4d9411a532716ce5e384dff0a3f
duonghovo <duonghovo@gmail.com>
2023-07-22 08:58:01 +0700

    Chon nha may

---
+++ src/pages/LoginSelect/LoginSelect.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Select, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import "./LoginSelect.css";
+
+const { Option } = Select;
+
+function LoginSelect() {
+  const factories = ["Nhà máy 1", "Nhà máy 2", "Nhà máy 3", "Nhà máy 4"];
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="login-select-container">
+      <h1 className="login-select-title">Hãy chọn nhà máy </h1>
+      <Select
+        className="login-select"
+        placeholder="Chọn nhà máy"
+        style={{ marginTop: "10px" }}
+      >
+        {factories.map((factory) => (
+          <Option value={factory}>{factory}</Option>
+        ))}
+      </Select>
+      <Button
+        className="login-button"
+        type="primary"
+        onClick={handleLoginClick}
+      >
+        Đăng nhập
+      </Button>
+    </div>
+  );
+}
+
+export default LoginSelect;
